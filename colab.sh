cb13158d159c972b9afc2258cd7b9cd6406301d1
Yuichi Sugita <34782651+cedro3@users.noreply.github.com>
2022-04-09 11:44:32 +0900

    Update colab.sh

--- colab.sh
+++ colab.sh
@@ -14,5 +14,6 @@ conda env create -f environment.yaml
 conda init bash
 source ~/.bashrc
 source activate icon
+pip install --upgrade gdown ###
 pip install -r requirements.txt --use-deprecated=legacy-resolver
 
